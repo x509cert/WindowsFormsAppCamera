dd0f10447e3c0b868db4d3a44b7a9fb82c5ce203
Michael Howard <mikehow@microsoft.com>
2021-04-11 12:27:10 -0500

    Refactored Form startup code

--- WindowsFormsAppCamera/Form1.cs
+++ WindowsFormsAppCamera/Form1.cs
@@ -336,113 +336,6 @@ namespace WindowsFormsAppCamera
 
         #region UI Elements
 
-        // Code to read command-line args
-        // -n "machinename" -c "connectionstring" -f "from sms #"  -t "to sms #" -u "log app URI"
-        bool GetCmdLineArgs(ref string machineName,         // -n
-                            ref string azureCommsString,    // -c
-                            ref string azureSmsFrom,        // -f
-                            ref string azureSmsTo,          // -t
-                            ref string logUri)              // -u 
-        {
-            bool success = true;
-
-            string[] args = Environment.GetCommandLineArgs();
-            if (args.Length == 0) return false;
-
-            try
-            {
-                for (int i = 1; i < args.Length; i++)
-                {
-                    if (args[i].ToLower().StartsWith("-n") == true)
-                    {
-                        machineName = args[i + 1];
-                        txtName.Text = machineName;
-                    }
-
-                    if (args[i].ToLower().StartsWith("-c") == true)
-                    {
-                        azureCommsString = args[i + 1];
-                    }
-
-                    if (args[i].ToLower().StartsWith("-f") == true)
-                    {
-                        azureSmsFrom = args[i + 1];
-                    }
-
-                    if (args[i].ToLower().StartsWith("-t") == true)
-                    {
-                        azureSmsTo = args[i + 1];
-                    }
-
-                    if (args[i].ToLower().StartsWith("-u") == true)
-                    {
-                        logUri = args[i + 1];
-                    }
-                }
-            }
-            catch (Exception)
-            {
-                success = false;
-            }
-
-            return success;
-        }
-
-        // The main entry point
-        public Form1()
-        {
-            InitializeComponent();
-
-            // logs go in user's profile folder (eg; c:\users\mikehow)
-            _sLogFilePath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
-
-            // get the date this binary was last created
-            string strpath = System.Reflection.Assembly.GetExecutingAssembly().Location;
-            System.IO.FileInfo fi = new System.IO.FileInfo(strpath);
-            string buildDate = fi.LastWriteTime.ToString("yyMMdd:HHmm");
-
-            // machine name
-            string machine = Dns.GetHostName();
-
-            // add info to title of the tools
-            Text = $"DivGrind [{buildDate}] on {machine}";
-
-            numTrigger.Value = (decimal)_triggerPercent;
-
-            _logQueue = new LogQueue(50);
-            
-            _machineName = Dns.GetHostName();
-
-            string machineName = "";
-            string azureConnection = "";
-            string azureSmsFrom = "";
-            string azureSmsTo = "";
-            string logUri = "";
-
-            bool ok = GetCmdLineArgs(ref machineName, ref azureConnection, ref azureSmsFrom, ref azureSmsTo, ref logUri);
-            if (ok)
-            {
-                // set machine name
-                if (string.IsNullOrEmpty(machineName) == false) _machineName = machineName;
-
-                // if the three args are available for SMS, then create an SmsAlert object
-                if (string.IsNullOrEmpty(azureConnection) == false &&
-                    string.IsNullOrEmpty(azureSmsFrom) == false &&
-                    string.IsNullOrEmpty(azureSmsTo) == false) 
-                {
-                    _smsAlert = new SmsAlert(machineName, azureConnection, azureSmsFrom, azureSmsTo) {
-                        BlockLateNightSms = true
-                    };
-
-                    txtSmsEnabled.Text = "Yes";
-                    btnTestSms.Enabled = true;
-                }
-
-                if (string.IsNullOrEmpty(logUri) == false)
-                    _logUri = logUri;
-            }
-        }
-
         // start drone monitoring
         private void btnStart_Click(object sender, EventArgs e)
         {
@@ -584,21 +477,6 @@ namespace WindowsFormsAppCamera
             // Nothing
         }
 
-        // this gives the code a chance to kill the main worker thread gracefully
-        private void Form1_Closing(object sender, FormClosingEventArgs e)
-        {
-            KillSkillTimer();
-            _fKillThreads = true;
-
-            Thread.Sleep(400);
-            e.Cancel = false;
-
-            if (_sComPort != null) { 
-                _sComPort.Close();
-                _sComPort = null;
-            }
-        }
-
         // this lets you fine-tune the % red increase to trigger the EMP (ie; 'Drones Incoming')
         private void numTrigger_ValueChanged(object sender, EventArgs e)
         {
@@ -685,12 +563,6 @@ namespace WindowsFormsAppCamera
                 return false;
 
             return true;
-
-            // if there is also an increase in blue and green, then it's an EMP flash
-            //float spottedGreen = (float)_calibrationAvg.G + (((float)_calibrationAvg.G / 100.0F) * _triggerPercent);
-            //float spottedBlue = (float)_calibrationAvg.B + (((float)_calibrationAvg.B / 100.0F) * _triggerPercent);
-
-            //return rbgTotal.G < spottedGreen || rbgTotal.B < spottedBlue;
         }
 
         // draws the yellow rectangle 'hitbox' -
@@ -708,7 +580,8 @@ namespace WindowsFormsAppCamera
                 g.DrawRectangle(_penYellow, rectTarget);
             }
         }
-        // counts the number of RBGA elements in pixels in the hitbox
+
+        // counts the number of RGB elements in pixels in the hitbox
         // skips every other pixel on the x-axis for perf
         private void GetRGBInRange(Bitmap bmp, ref RGBTotal rbgTotal)
         {
---
+++ WindowsFormsAppCamera/FormStartStop.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Threading;
+using System.Net;
+using System.IO;
+
+namespace WindowsFormsAppCamera
+{
+    public partial class Form1 : Form
+    {
+        // The main entry point
+        public Form1()
+        {
+            InitializeComponent();
+
+            // logs go in user's profile folder (eg; c:\users\mikehow)
+            _sLogFilePath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+
+            // get the date this binary was last modified
+            string strpath = System.Reflection.Assembly.GetExecutingAssembly().Location;
+            FileInfo fi = new FileInfo(strpath);
+            string buildDate = fi.LastWriteTime.ToString("yyMMdd:HHmm");
+
+            // machine name
+            string machine = Dns.GetHostName();
+
+            // add info to title of the tools
+            Text = $"DivGrind [{buildDate}] on {machine}";
+
+            numTrigger.Value = (decimal)_triggerPercent;
+
+            _logQueue = new LogQueue(50);
+
+            _machineName = Dns.GetHostName();
+
+            string machineName = "";
+            string azureConnection = "";
+            string azureSmsFrom = "";
+            string azureSmsTo = "";
+            string logUri = "";
+
+            bool ok = GetCmdLineArgs(ref machineName, ref azureConnection, ref azureSmsFrom, ref azureSmsTo, ref logUri);
+            if (ok)
+            {
+                // set machine name
+                if (string.IsNullOrEmpty(machineName) == false) _machineName = machineName;
+
+                // if the three args are available for SMS, then create an SmsAlert object
+                if (string.IsNullOrEmpty(azureConnection) == false &&
+                    string.IsNullOrEmpty(azureSmsFrom) == false &&
+                    string.IsNullOrEmpty(azureSmsTo) == false)
+                {
+                    _smsAlert = new SmsAlert(machineName, azureConnection, azureSmsFrom, azureSmsTo)
+                    {
+                        BlockLateNightSms = true
+                    };
+
+                    txtSmsEnabled.Text = "Yes";
+                    btnTestSms.Enabled = true;
+                }
+
+                if (string.IsNullOrEmpty(logUri) == false)
+                    _logUri = logUri;
+            }
+        }
+
+        // Code to read command-line args
+        // -n "machinename" -c "connectionstring" -f "from sms #"  -t "to sms #" -u "log app URI"
+        bool GetCmdLineArgs(ref string machineName,         // -n
+                            ref string azureCommsString,    // -c
+                            ref string azureSmsFrom,        // -f
+                            ref string azureSmsTo,          // -t
+                            ref string logUri)              // -u 
+        {
+            bool success = true;
+
+            string[] args = Environment.GetCommandLineArgs();
+            if (args.Length == 0) return false;
+
+            try
+            {
+                for (int i = 1; i < args.Length; i++)
+                {
+                    if (args[i].ToLower().StartsWith("-n") == true)
+                        txtName.Text = machineName = args[i + 1];
+
+                    if (args[i].ToLower().StartsWith("-c") == true)
+                        azureCommsString = args[i + 1];
+
+                    if (args[i].ToLower().StartsWith("-f") == true)
+                        azureSmsFrom = args[i + 1];
+
+                    if (args[i].ToLower().StartsWith("-t") == true)
+                        azureSmsTo = args[i + 1];
+
+                    if (args[i].ToLower().StartsWith("-u") == true)
+                        logUri = args[i + 1];
+                }
+            }
+            catch (Exception)
+            {
+                success = false;
+            }
+
+            return success;
+        }
+
+        // this gives the code a chance to kill the main worker thread gracefully
+        private void Form1_Closing(object sender, FormClosingEventArgs e)
+        {
+            KillSkillTimer();
+            _fKillThreads = true;
+
+            Thread.Sleep(400);
+            e.Cancel = false;
+
+            if (_sComPort != null)
+            {
+                _sComPort.Close();
+                _sComPort = null;
+            }
+        }
+    }
+}
+
+
